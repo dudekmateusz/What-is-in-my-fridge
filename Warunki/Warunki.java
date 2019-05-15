df3fc43275428f3e5827824ddac05ff0cb00eea6
Mateusz Dudek <50460337+dudekmateusz@users.noreply.github.com>
2019-05-15 10:52:22 +0200

    Conditions
    Here are conditions to program which solves tasks.

---
+++ Warunki/Warunki.java
@@ -0,0 +1,8 @@
+package Warunki;
+
+public class Warunki {
+
+    //tu mają być warunki, które sprawią, że jeśli podamy produkty, które mamy w lodówce, to na podstawie minimum 3 produktów podstawowych
+    //wyświetlą się proponowane przepisy, które mają je w swojej bazie.
+
+}
